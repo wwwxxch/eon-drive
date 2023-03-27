38bbda4aaa6f2c38664915d1a52f1c25bfdbe22b
Chih-Hui Wang <chihhui.x@gmail.com>
2023-03-27 11:32:20 +0800

    [build] project init

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config();
+const port = process.env.PORT;
+
+app.use(express.static("./public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ---------------------------------------------------
+// Simple check
+
+app.get("/check", (req, res) => {
+  console.log("/check");
+  return res.send("Hello World!");
+});
+
+// ---------------------------------------------------
+// Errors
+
+app.use((req, res, next) => {
+  console.log("ERROR req.path: ", req.path);
+  const err = new Error("=====Page not found=====");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {  
+  res.status(err.status || 500)
+    .json({ 
+      status: err.status,
+      message: err.message,
+      stack: err.stack 
+    });
+});
+
+// ---------------------------------------------------
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
