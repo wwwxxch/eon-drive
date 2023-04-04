86998247caf61e14295127c75c9fa7c99ea634ed
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-04 19:25:42 +0800

    [feat] Add function for uploading large file via multipart to S3

--- server/util/s3_upload.js
+++ server/util/s3_upload.js
@@ -1,16 +1,18 @@
-import { 
-  S3Client, 
-  PutObjectCommand,
-  CreateMultipartUploadCommand,
-  CompleteMultipartUploadCommand,
-  UploadPartCommand 
+import {
+	S3Client,
+	PutObjectCommand,
+	CreateMultipartUploadCommand,
+	CompleteMultipartUploadCommand,
+	UploadPartCommand,
+	AbortMultipartUploadCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+import fs from "fs";
 
 import dotenv from "dotenv";
 dotenv.config();
 const {
-	// S3_BUCKET_NAME,
 	S3_BUCKET_REGION,
 	S3_ACCESS_KEY_ID,
 	S3_SECRET_ACCESS_KEY,
@@ -22,11 +24,16 @@ const config = {
 		secretAccessKey: S3_SECRET_ACCESS_KEY,
 	},
 	region: S3_BUCKET_REGION,
+  logger: {
+    log: (args) => console.log(args)
+  }
 };
 
 const client = new S3Client(config);
 
-// Single File
+// ==================================================================
+
+// get the presigned URL for an entire file upload
 async function getSingleSignedUrl(bucket, fileName, expiresIn) {
 	const command = new PutObjectCommand({
 		Bucket: bucket,
@@ -36,31 +43,160 @@ async function getSingleSignedUrl(bucket, fileName, expiresIn) {
 	return await getSignedUrl(client, command, { expiresIn });
 }
 
-// Multi Files
-
 // get the pre-signed URL for completing a multipart upload
 async function getCompleteUrl(bucket, fileName, uploadId, expiresIn) {
-  const command = new CompleteMultipartUploadCommand({
-    Bucket: bucket,
-    Key: fileName,
-    UploadId: uploadId
-  });
-  return await getSignedUrl(client, command, { expiresIn });
+	const command = new CompleteMultipartUploadCommand({
+		Bucket: bucket,
+		Key: fileName,
+		UploadId: uploadId,
+	});
+	return await getSignedUrl(client, command, { expiresIn });
 }
 
 // get the pre-signed URL for a single part upload
 async function getPartUrl(bucket, fileName, uploadId, partNumber, expiresIn) {
-  const command = new UploadPartCommand({
-    Bucket: bucket,
-    Key: fileName,
-    UploadId: uploadId,
-    PartNumber: partNumber
-  });
-  return await getSignedUrl(client, command, { expiresIn });
+	const command = new UploadPartCommand({
+		Bucket: bucket,
+		Key: fileName,
+		UploadId: uploadId,
+		PartNumber: partNumber,
+	});
+	return await getSignedUrl(client, command, { expiresIn });
 }
 
-export { 
-  getSingleSignedUrl,
-  getCompleteUrl,
-  getPartUrl
+// upload large file
+const largeUpload_notused = async (bucket, fileName, localPath, fileSize) => {
+    // Create Multipart Upload Command
+		const cmdCreateMultipartUpload = new CreateMultipartUploadCommand({
+			Bucket: bucket,
+			Key: fileName,
+		});
+		const multipartUpload = await client.send(cmdCreateMultipartUpload);
+    
+    // Upload Part
+		let uploadId = multipartUpload.UploadId;
+  try {
+		const uploadPromises = [];
+		const partCount = Math.ceil(fileSize / (5 * 1024 * 1024));
+		for (let i = 0; i < partCount; i++) {
+			const start = i * (5 * 1024 * 1024);
+			const end = Math.min(start + 5 * 1024 * 1024, fileSize);
+			const cmdUploadPart = new UploadPartCommand({
+				Bucket: bucket,
+				Key: fileName,
+				UploadId: uploadId,
+				Body: fs.createReadStream(localPath, { start, end }),
+				ContentLength: end - start,
+				PartNumber: i + 1,
+			});
+			uploadPromises.push(
+				client.send(cmdUploadPart).then((data) => {
+					console.log("Part", i + 1, "uploaded");
+					return data;
+				})
+			);
+		}
+
+    // Complete Multipart
+		const uploadResults = await Promise.all(uploadPromises);
+		const cmdCompleteMultipart = new CompleteMultipartUploadCommand({
+			Bucket: bucket,
+			Key: fileName,
+			UploadId: uploadId,
+			MultipartUpload: {
+				Parts: uploadResults.map(({ ETag }, i) => ({
+					ETag: ETag,
+					PartNumber: i + 1,
+				})),
+			},
+		});
+		return await client.send(cmdCompleteMultipart);
+	} catch (e) {
+		console.error("largeUpload: ", e);
+
+    // Abort Multipart
+		if (uploadId) {
+			const abortCommand = new AbortMultipartUploadCommand({
+				Bucket: bucket,
+				Key: fileName,
+				UploadId: uploadId,
+			});
+			const abort = await client.send(abortCommand);
+      console.log("abort: ", abort);
+		}
+	}
 };
+
+const largeUpload = async (bucket, fileName, localPath, fileSize) => {
+	// Create Multipart Upload Command
+	const cmdCreateMultipartUpload = new CreateMultipartUploadCommand({
+		Bucket: bucket,
+		Key: fileName,
+	});
+	const multipartUpload = await client.send(cmdCreateMultipartUpload);
+
+	// Upload Part
+	let uploadId = multipartUpload.UploadId;
+	try {
+		const uploadPromises = [];
+		const partCount = Math.ceil(fileSize / (5 * 1024 * 1024));
+		for (let i = 0; i < partCount; i++) {
+			const start = i * (5 * 1024 * 1024);
+			const end = Math.min(start + 5 * 1024 * 1024, fileSize);
+			const cmdUploadPart = new UploadPartCommand({
+				Bucket: bucket,
+				Key: fileName,
+				UploadId: uploadId,
+				Body: fs.createReadStream(localPath, { start, end }),
+				ContentLength: end - start,
+				PartNumber: i + 1,
+			});
+			uploadPromises.push(
+				client.send(cmdUploadPart).then((data) => {
+					console.log("Part", i + 1, "uploaded");
+					return data;
+				})
+			);
+		}
+
+		// Complete Multipart
+		const completeUrl = await getCompleteUrl(bucket, fileName, uploadId, 3600);
+		const uploadResults = await Promise.all(uploadPromises);
+		const xmlBody = `
+      <CompleteMultipartUpload>
+        ${uploadResults
+					.map((item, i) => {
+						return `
+            <Part>
+              <PartNumber>${i + 1}</PartNumber>
+              <ETag>${item.ETag.replace(/"/g, "")}</ETag>
+            </Part>
+          `;
+					})
+					.join("")}
+      </CompleteMultipartUpload>
+    `;
+		const completeMultipart = await fetch(completeUrl, {
+			method: "POST",
+			headers: { "Content-Type": "application/xml" },
+			body: xmlBody,
+		});
+		// console.log(completeMulripart);
+		return completeMultipart.status;
+	} catch (e) {
+		console.error("largeUpload: ", e);
+
+		// Abort Multipart
+		if (uploadId) {
+			const abortCommand = new AbortMultipartUploadCommand({
+				Bucket: bucket,
+				Key: fileName,
+				UploadId: uploadId,
+			});
+			const abort = await client.send(abortCommand);
+			console.log("abort: ", abort);
+		}
+	}
+};
+
+export { getSingleSignedUrl, getCompleteUrl, getPartUrl, largeUpload };
