5b3509511896caaad079bd5c700132cb1e48781c
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-09 00:31:07 +0800

    [feat] Calculate storage usage

--- server/model/db_user.js
+++ server/model/db_user.js
@@ -1,9 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import { pool } from "./connection.js";
-
 import argon2  from "argon2";
+import { pool } from "./connection.js";
 // ----------------------------------------------------------
 const chkmail = async (mail) => {
   const [row] = await pool.query(`
@@ -17,12 +13,14 @@ const chkpair = async (mail, pwd) => {
     SELECT email, password FROM user WHERE email = ?`, mail
 	);
 	if (row.length > 0) {
-		const validate = await argon2.verify(row[0].password, pwd); //(hashed value, plain text)
+    // argon2.verify(<hashed value>, <plain text>)
+		const validate = await argon2.verify(row[0].password, pwd); 
 		return validate; // possible value: true, false
 	}
 	return false;
 };
 
+// get user info by specific column
 const getUser = async (col, colVal) => {
   const [row] = await pool.query(`
     SELECT id, email, name, allocated, used FROM user WHERE ${col} = ?`, colVal
@@ -30,11 +28,11 @@ const getUser = async (col, colVal) => {
   return row[0];
 };
 
-const createUser = async(mail, pwd, name) => {
+const createUser = async (mail, pwd, name) => {
   const hashed = await argon2.hash(pwd);
   const [row] = await pool.query(`
     INSERT INTO user (email, password, name, plan, allocated, used) 
-    VALUES (?, ?, ?, ?, ?, ?)`, [mail, hashed, name, 1, 1*1024*1024, 0]
+    VALUES (?, ?, ?, ?, ?, ?)`, [mail, hashed, name, 1,  400 * 1024 * 1024, 0]
   );
   const insertId = row.insertId;
   return getUser("id", insertId);
