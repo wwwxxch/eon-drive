d7bc2c1c3d09c61a894914cb59ff3ac626098fd7
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-09 00:31:53 +0800

    [refactor]

--- server/util/util.js
+++ server/util/util.js
@@ -1,8 +1,3 @@
-import { 
-  getDirId,
-  getFileList 
-} from "../model/db_file.js";
-
 import fs from "fs";
 //==============================================================================
 const wrapAsync = (fn) => {
@@ -14,56 +9,6 @@ const wrapAsync = (fn) => {
   };
 };
 
-const iterPath = async (userId, path) => {
-  const split = path.split("/");
-  let parentId = 0;
-  for (let i = 0; i < split.length; i++) {
-    const chkDir = await getDirId(userId, parentId, split[i]);
-    if (chkDir.length > 0) {
-      parentId = chkDir[0].id;
-    }
-  }
-  // console.log(parentId);
-  return parentId;
-};
-
-const recur = async (userId, parentId, result, currentpath) => {
-  const arr = await getFileList(userId, parentId);
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].type === "file") {
-      result.push(`${currentpath}${arr[i].name}`);
-    } else {
-      await recur(userId, arr[i].id, result, currentpath + arr[i].name);
-    }
-  }
-};
-
-const getWholeChilds = async (userId, path) => {
-  console.log("getWholeChilds: path: ", path);
-  const result = [];
-  const parentId = await iterPath(userId, path);
-  let currentpath = path;
-
-  await recur(userId, parentId, result, currentpath);
-  // console.log("result: ", result);
-  return result;
-};
-
-const getFileListByPath = async (userId, path) => {
-  let parentId = 0;
-  if (path !== "") {
-    const folders = path.split("/");
-    for (let i = 0; i < folders.length; i++) {
-      const chkDir = await getDirId(userId, parentId, folders[i]);
-      if (chkDir.length === 0) {
-        return { data: null };
-      }
-      parentId = chkDir[0].id;
-    }
-  }
-  const list = await getFileList(userId, parentId);
-  return { data: list };
-};
 
 const deleteLocal = async (localPath) => {
   try {
@@ -77,7 +22,5 @@ const deleteLocal = async (localPath) => {
 
 export {
   wrapAsync,
-  getWholeChilds,
-  deleteLocal,
-  getFileListByPath
+  deleteLocal
 };
