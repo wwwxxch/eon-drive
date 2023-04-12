2d4747b5eb70ad02e93d2bde8ea22461a7c28a5a
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-12 11:53:37 +0800

    [chore] update route to make distinguish

--- server/route/file/file_delete_route.js
+++ server/route/file/file_delete_route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 import { authentication } from "../../controller/user/user_auth_controller.js";
 import { deleteS3AndDB } from "../../controller/file/file_delete_controller.js";
 // ------------------------------------------------------------------------------------
-router.post("/v2/delete", authentication, deleteS3AndDB);
+router.post("/v1/delete", authentication, deleteS3AndDB);
 
 export { router as file_delete_route };
