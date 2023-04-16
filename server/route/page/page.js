5d72bfbaefe00935e2dccd043cc5a8c14350941b
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-17 00:01:37 +0800

    [feat] add route for file history

--- server/route/page/page.js
+++ server/route/page/page.js
@@ -7,6 +7,9 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+import { findFileIdByPath } from "../../service/path/iter.js";
+import { getVersionsByFileId } from "../../model/db_ff_r.js";
+import { getDeleteRecordsByFileId } from "../../model/db_ff_r.js";
 // =========================================================
 router.get("/", (req, res) => {
   return res.sendFile(path.join(__dirname, "../../../views", "index.html"));
@@ -24,8 +27,27 @@ router.get(/^\/home(\/.*)?$/, (req, res) => {
   return res.sendFile(path.join(__dirname, "../../../views", "home.html"));
 });
 
-router.get("/history/*", (req, res) => {
-  return res.sendFile(path.join(__dirname, "../../../views", "history.html"));
+router.get("/history/*", async (req, res) => {
+  console.log(req.path);
+  const fileWholePath = req.path.replace(/^\/history\//,"");
+  const fileName = fileWholePath.split("/").pop();
+  const userId = req.session.user.id;
+  
+  const fileId = await findFileIdByPath(userId, fileWholePath);
+  console.log("fileId: ", fileId);
+  if (!fileId) {
+    return res.staatus(404).send("404");
+  }
+  
+  // const versions = await getVersionsByFileId(fileId);
+  // console.log("versions", versions);
+
+  // const deleteRecords = await getDeleteRecordsByFileId(fileId);
+  // console.log("deleteRecords: ", deleteRecords); 
+  
+  // return res.json({ versions, deleteRecords });
+  // return res.sendFile(path.join(__dirname, "../../../views", "history.html"));
+  return res.render("history", { fileName });
 });
 
 router.get("/trash", (req, res) => {
