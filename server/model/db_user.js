1db25af1ec51f9da111207df1ab7072df857c765
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-20 23:48:11 +0800

    [feat] possible email list API

--- server/model/db_user.js
+++ server/model/db_user.js
@@ -1,4 +1,4 @@
-import argon2  from "argon2";
+import argon2 from "argon2";
 import { pool } from "./connection.js";
 
 import dotenv from "dotenv";
@@ -8,20 +8,24 @@ dotenv.config();
 const basic = parseInt(process.env.BASIC);
 // ----------------------------------------------------------
 const chkmail = async (mail) => {
-  const [row] = await pool.query(`
-    SELECT email FROM user WHERE email = ?`, mail
-  );
-  return row[0];
+	const [row] = await pool.query(
+		`
+    SELECT email FROM user WHERE email = ?`,
+		mail
+	);
+	return row[0];
 };
 
 const chkpair = async (mail, pwd) => {
-  console.log("chkpair");
-  const [row] = await pool.query(`
-    SELECT email, password FROM user WHERE email = ?`, mail
+	console.log("chkpair");
+	const [row] = await pool.query(
+		`
+    SELECT email, password FROM user WHERE email = ?`,
+		mail
 	);
 	if (row.length > 0) {
-    // argon2.verify(<hashed value>, <plain text>)
-		const validate = await argon2.verify(row[0].password, pwd); 
+		// argon2.verify(<hashed value>, <plain text>)
+		const validate = await argon2.verify(row[0].password, pwd);
 		return validate; // possible value: true, false
 	}
 	return false;
@@ -29,28 +33,54 @@ const chkpair = async (mail, pwd) => {
 
 // get user info by specific column
 const getUser = async (col, colVal) => {
-  const [row] = await pool.query(`
-    SELECT id, email, name, allocated, used FROM user WHERE ${col} = ?`, colVal
-  );
-  return row[0];
+	const [row] = await pool.query(
+		`
+    SELECT id, email, name, allocated, used FROM user WHERE ${col} = ?`,
+		colVal
+	);
+	return row[0];
 };
 
 const createUser = async (mail, pwd, name, time) => {
-  const hashed = await argon2.hash(pwd);
-  const [row] = await pool.query(`
+	const hashed = await argon2.hash(pwd);
+	const [row] = await pool.query(
+		`
     INSERT INTO user (email, password, name, plan, allocated, used, created_at, updated_at) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, [mail, hashed, name, 1,  basic * 1024 * 1024, 0, time, time]
-  );
-  const insertId = row.insertId;
-  return getUser("id", insertId);
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+		[mail, hashed, name, 1, basic * 1024 * 1024, 0, time, time]
+	);
+	const insertId = row.insertId;
+	return getUser("id", insertId);
 };
 
 const getMultipleUserId = async (col, vals) => {
-  const vals_trim = vals.map(item => item.trim());
-  const [row] = await pool.query(`
-    SELECT id FROM user WHERE ${col} IN (?)`, [vals_trim]);
-  console.log("row: ", row.map(item => item.id));
-  return row.map(item => item.id);
+	const vals_trim = vals.map((item) => item.trim());
+	const [row] = await pool.query(
+		`
+    SELECT id FROM user WHERE ${col} IN (?)`,
+		[vals_trim]
+	);
+	console.log("row: ", row.map((item) => item.id));
+	return row.map((item) => item.id);
 };
 
-export { chkmail, chkpair, getUser, createUser, getMultipleUserId };
+const getPossibleUser = async (str) => {
+	const strPlusWlidcard = str + "%";
+	const [row] = await pool.query(
+		`
+    SELECT email FROM user WHERE email LIKE ?
+  `,
+		strPlusWlidcard
+	);
+
+	return row.map((item) => item.email);
+};
+
+export {
+	chkmail,
+	chkpair,
+	getUser,
+	createUser,
+	getMultipleUserId,
+	getPossibleUser,
+};
--- server/route/link/link_manage_route.js
+++ server/route/link/link_manage_route.js
@@ -10,9 +10,21 @@ import {
 	publicLink,
 } from "../../controller/link/link_create_controller.js";
 import { revokeLink } from "../../controller/link/link_revoke_controller.js";
+import { getPossibleUser } from "../../model/db_user.js";
 // --------------------------------------------------------------------------------
 router.post("/create-link", authentication, publicLink, privateLink);
 
 router.post("/revoke-link", authentication, revokeLink);
+
+router.get("/select-user", authentication, async(req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.json({ msg: "q is missing" });
+  } 
+
+  const possibleEmail = await getPossibleUser(q);
+  
+  return res.json({ list: possibleEmail });
+});
 
 export { router as link_manage_route };
