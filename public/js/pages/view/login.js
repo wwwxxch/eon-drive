7a8845b33d49037d903ed1f0f92bac3ab146efaf
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-26 19:55:23 +0800

    [feat] FE - input validation

--- public/js/pages/view/login.js
+++ public/js/pages/view/login.js
@@ -1,9 +1,10 @@
 const loginReq = async () => {
+  $("#login-err-msg").empty();
   const email = $("#email").val();
   const password = $("#password").val();
 
   try {
-    const loginRes = await axios.post("/signin", { email: email, password: password });
+    const loginRes = await axios.post("/signin", { email, password });
     if (loginRes.status === 200) {
       // console.log("login success");
       window.location.href="/home";
@@ -11,7 +12,15 @@ const loginReq = async () => {
   } catch (err) {
     // console.error("loginReq: ", err);
     $("#login-failed-modal").modal("show");
-    $("#login-err-msg").text(err.response.data.error);
+    if (typeof err.response.data.error === "string") {
+      $("#login-err-msg").text(err.response.data.error);
+    } else {
+
+      err.response.data.error.forEach((item) => {
+        $("#login-err-msg").append(`<div>${item}</div>`);
+      }); 
+    }
+    
   }
 };
 
--- public/js/pages/view/register.js
+++ public/js/pages/view/register.js
@@ -1,4 +1,5 @@
 const registerReq = async () => {
+  $("#login-err-msg").empty();
   const name = $("#username").val();
   const email = $("#email").val();
   const password = $("#password").val();
@@ -10,9 +11,16 @@ const registerReq = async () => {
       window.location.href="/home";
     }
   } catch (err) {
-    console.error("registerReq: ", err);
+    // console.error("registerReq: ", err);
     $("#login-failed-modal").modal("show");
-    $("#login-err-msg").text(err.response.data.error);
+    if (typeof err.response.data.error === "string") {
+      $("#login-err-msg").text(err.response.data.error);
+    } else {
+
+      err.response.data.error.forEach((item) => {
+        $("#login-err-msg").append(`<div class="pb-1">${item}</div>`);
+      }); 
+    }
   }
 };
 
