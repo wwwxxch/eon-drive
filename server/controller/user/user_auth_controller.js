ef10255ad76fa2d695e24a37c807349aef185f3c
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-28 17:22:51 +0800

    [feat] change the way response error the FE

--- server/controller/user/user_auth_controller.js
+++ server/controller/user/user_auth_controller.js
@@ -2,141 +2,142 @@ import { DateTime } from "luxon";
 
 import dotenv from "dotenv";
 dotenv.config();
-const {	
-  S3_MAIN_BUCKET_NAME,
-  S3_DOWNLOAD_BUCKET_NAME
-} = process.env;
+const { S3_MAIN_BUCKET_NAME, S3_DOWNLOAD_BUCKET_NAME } = process.env;
+
+import { customError } from "../../error/custom_error.js";
 
 import {
-  chkmail, 
-  createUser,
-  chkpair, 
-  getUser,
-  getProfile
+	chkmail,
+	createUser,
+	chkpair,
+	getUser,
+	getProfile,
 } from "../../model/db_user.js";
 
-import {
-  s3clientGeneral
-} from "../../service/s3/s3_client.js";
+import { s3clientGeneral } from "../../service/s3/s3_client.js";
 
-import {
-  createS3Folder
-} from "../../service/s3/s3_create.js";
+import { createS3Folder } from "../../service/s3/s3_create.js";
 
 // ====================================================================
-const signUp = async (req, res) => {
-  console.log("signUp");
-  // console.log("signUp: ", req.body);
+const signUp = async (req, res, next) => {
+	console.log("signUp");
 
-  const { name, email, password } = req.body;
+	const { name, email, password } = req.body;
 
-  // check if the email has been registered
-  const getmail = await chkmail(email);
-  if (getmail) {
-    return res.status(400).json({ error: "Your email has been registered." });
-  }
+	// check if the email has been registered
+	const getmail = await chkmail(email);
+	if (getmail) {
+		// return res.status(400).json({ error: "Your email has been registered." });
+		return next(customError.badRequest("Your email has been registered"));
+	}
 
-  // create user in DB
-  const now = DateTime.utc();
-  const nowTime = now.toFormat("yyyy-MM-dd HH:mm:ss");
+	// create user in DB
+	const now = DateTime.utc();
+	const nowTime = now.toFormat("yyyy-MM-dd HH:mm:ss");
 
-  const createUserRes = await createUser(email, password, name, nowTime);
-  
-  // save user info to session
-  const user = {
-      id: createUserRes.id,
-      name: createUserRes.name,
-      email: createUserRes.email,
-      allocated: createUserRes.allocated,
-      used: createUserRes.used
-  };
-  req.session.user = user;
+	const createUserRes = await createUser(email, password, name, nowTime);
 
-  // create blank folder in S3
-  const createMain = await createS3Folder(s3clientGeneral, S3_MAIN_BUCKET_NAME, `user_${createUserRes.id}`);
-  const createDownload = await createS3Folder(s3clientGeneral, S3_DOWNLOAD_BUCKET_NAME, `user_${createUserRes.id}`);
-  console.log("signUp: createMain: ", createMain);
-  console.log("signUp: createDownload: ", createDownload);
+	// save user info to session
+	const user = {
+		id: createUserRes.id,
+		name: createUserRes.name,
+		email: createUserRes.email,
+		allocated: createUserRes.allocated,
+		used: createUserRes.used,
+	};
+	req.session.user = user;
 
-  if (createMain["$metadata"].httpStatusCode !== 200 || createDownload["$metadata"].httpStatusCode !== 200) {
-    return res.status(500).json({ msg: "Something Wrong" });
-  }
+	// create blank folder in S3
+	const createMain = await createS3Folder(
+		s3clientGeneral,
+		S3_MAIN_BUCKET_NAME,
+		`user_${createUserRes.id}`
+	);
+	const createDownload = await createS3Folder(
+		s3clientGeneral,
+		S3_DOWNLOAD_BUCKET_NAME,
+		`user_${createUserRes.id}`
+	);
+	console.log("signUp: createMain: ", createMain);
+	console.log("signUp: createDownload: ", createDownload);
 
-  return res.json({ data: { user } });
+	if (!createMain || !createDownload) {
+		// return res.status(500).json({ msg: "Something Wrong" });
+    return next(customError.internalServerError());
+	}
+
+	return res.json({ data: { user } });
 };
 
-const signIn = async (req, res) => {
-  console.log("signIn");
-  // console.log("signIn: ", req.body);
+const signIn = async (req, res, next) => {
+	console.log("signIn");
+	const { email, password } = req.body;
 
-  const { email, password } = req.body;
+	// check if the email matches the password
+	const getpair = await chkpair(email, password);
+	if (!getpair) {
+		// return res.status(401).json({ error: "Your email and password do not match." });
+    return next(customError.unauthorized("Your email and password do not match"));
+	}
 
-  // check if the email matches the password
-  const getpair = await chkpair(email, password);
-  if (!getpair) {
-    return res.status(401).json({ error: "Your email and password do not match." });
-  }
+	// save user info to session
+	const getUserRes = await getUser("email", email);
+	console.log("getUserRes: ", getUserRes);
+	const user = {
+		id: getUserRes.id,
+		name: getUserRes.name,
+		email: getUserRes.email,
+		allocated: getUserRes.allocated,
+		used: getUserRes.used,
+	};
+	req.session.user = user;
 
-  // save user info to session
-  const getUserRes = await getUser("email", email);
-  console.log("getUserRes: ", getUserRes);
-  const user = {
-    id: getUserRes.id,
-    name: getUserRes.name,
-    email: getUserRes.email,
-    allocated: getUserRes.allocated,
-    used: getUserRes.used
-  };
-  req.session.user = user;
-  
-  return res.json({ data: { user } });
+	return res.json({ data: { user } });
 };
 
 const logOut = (req, res) => {
-  console.log("logOut");
-  req.session.destroy();
-  // return res.json({ msg: "logout" });
-  return res.redirect("/");
+	console.log("logOut");
+	req.session.destroy();
+	// return res.json({ msg: "logout" });
+	return res.redirect("/");
 };
 
 const showProfile = async (req, res) => {
-  console.log("showProfile");
-  const userId = req.session.user.id;
-  const profile = await getProfile(userId);
-  const { email, name, plan, allocated, used, created_at } = profile;
-    
-  return res.json({ email, name, plan, allocated, used, created_at });
+	console.log("showProfile");
+	const userId = req.session.user.id;
+	const profile = await getProfile(userId);
+	const { email, name, plan, allocated, used, created_at } = profile;
+
+	return res.json({ email, name, plan, allocated, used, created_at });
 };
 
 const authentication = (req, res, next) => {
-  if (!req.session.user) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-  next();
+	if (!req.session.user) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+	next();
 };
 
 const loginRedirect = (req, res, next) => {
-  if (req.session.user) {
-    return res.redirect("/home");
-  }
-  next();
+	if (req.session.user) {
+		return res.redirect("/home");
+	}
+	next();
 };
 
 const pageAuth = (req, res, next) => {
-  if (!req.session.user) {
-    return res.redirect("/");
-  }
-  next();
+	if (!req.session.user) {
+		return res.redirect("/");
+	}
+	next();
 };
 
-// TODO: check where to put authentication, pageAuth, loginRedirect
-
 export {
-  signUp,
-  signIn,
-  logOut,
-  showProfile,
-  authentication,
-  pageAuth,
-  loginRedirect
+	signUp,
+	signIn,
+	logOut,
+	showProfile,
+	authentication,
+	pageAuth,
+	loginRedirect,
 };
