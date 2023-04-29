2201e51e3674f63030b8a439d0f466b2c0cd2de0
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-29 22:39:04 +0800

    [feat] add error responses in FE

--- public/js/pages/visitor/view_file.js
+++ public/js/pages/visitor/view_file.js
@@ -1,24 +1,48 @@
 import { downloadShareFi } from "../../api/view.js";
 
 // download file
-$(".fi-dl-btn").on("click", async function() {
-  const windowPathName = window.location.pathname;
+$(".fi-dl-btn").on("click", async function () {
+	const windowPathName = window.location.pathname;
 	const shareToken = windowPathName.split("/")[3];
 	console.log(shareToken);
-  
-  const downloadModal = $("#waitingModal");
-	const downloadStatus = $(".waiting-status");
-  downloadModal.modal("show");
-  downloadStatus.text("Downloading...");
-  $("#waiting-spinner").addClass("spinner-border");
-  $(".waiting-complete").hide();
 
-  const downloadFileRes = await downloadShareFi(shareToken);
-  if (downloadFileRes.status === 200) {
-    $("#waiting-spinner").removeClass("spinner-border");
-    $(".waiting-complete").show();
-    setTimeout(() => downloadStatus.text("Complete!"), 200);
-    setTimeout(() => downloadModal.modal("hide"), 1500);
-		window.open(downloadFileRes.downloadUrl, "_blank");
+	const downloadModal = $("#waitingModal");
+	const downloadStatus = $("#waiting-status");
+	const downloadSpinner = $("#waiting-spinner");
+	const downloadComplete = $("#waiting-complete");
+	const downloadError = $("#waiting-error");
+
+	downloadModal.modal("show");
+	downloadStatus.text("Downloading...");
+	downloadSpinner.addClass("spinner-border");
+	downloadComplete.hide();
+  downloadError.html();
+
+	const downloadFileRes = await downloadShareFi(shareToken);
+	if (downloadFileRes.status === 200) {
+		downloadSpinner.removeClass("spinner-border");
+		setTimeout(() => downloadStatus.text("Complete!"), 100);
+		setTimeout(() => downloadModal.modal("hide"), 200);
+		window.open(downloadFileRes.downloadUrl, "_self");
+		return;
+	} else if (downloadFileRes.status !== 500) {
+		let errorHTML;
+		if (typeof downloadFileRes.data.error === "string") {
+			errorHTML = `<span>${downloadFileRes.data.error}</span>`;
+		} else {
+			errorHTML = downloadFileRes.data.error
+				.map((err) => `<span>${err}</span>`)
+				.join("");
+		}
+		downloadSpinner.removeClass("spinner-border");
+		downloadStatus.text("");
+		downloadError.html(errorHTML);
+	} else {
+		const errorHTML =
+			"<span>Opps! Something went wrong. Please try later or contact us.</span>";
+		downloadSpinner.removeClass("spinner-border");
+		downloadStatus.text("");
+		downloadError.html(errorHTML);
 	}
+	setTimeout(() => downloadModal.modal("hide"), 2000);
 });
