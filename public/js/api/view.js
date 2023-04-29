5b56dae5f61b5ec78e5a9b622efb39475188c828
Chih-Hui Wang <chihhui.x@gmail.com>
2023-04-29 23:26:56 +0800

    [feat] update the response of api call function

--- public/js/api/view.js
+++ public/js/api/view.js
@@ -6,7 +6,7 @@ const getShareFoList = async(shareToken, subFolder = null) => {
 		return getList.data;
 	} catch (e) {
 		console.error("getShareFoList: ", e);
-		return false;
+		return null;
 	}
 };
 
@@ -14,10 +14,10 @@ const downloadShareFo = async(shareToken, desired) => {
   try {
 		const downloadRes = await axios.post("/view-fo-dl", { shareToken, desired });
 		console.log("downloadRes: ", downloadRes);
-    return { status: 200, downloadUrl: downloadRes.data.downloadUrl } ;
+    return { status: downloadRes.status, downloadUrl: downloadRes.data.downloadUrl } ;
 	} catch (e) {
 		console.error("downloadShare: ", e);
-		return false;
+		return { status: e.response.status, data: e.response.data };
 	}
 };
 
@@ -25,10 +25,10 @@ const downloadShareFi = async(shareToken) => {
   try {
 		const downloadRes = await axios.post("/view-fi-dl", { shareToken });
 		console.log("downloadRes: ", downloadRes);
-    return { status: 200, downloadUrl: downloadRes.data.downloadUrl } ;
+    return { status: downloadRes.status, downloadUrl: downloadRes.data.downloadUrl } ;
 	} catch (e) {
 		console.error("downloadShare: ", e);
-		return false;
+		return { status: e.response.status, data: e.response.data };
 	}
 };
 
