e1232bf61ce624451eb9105f78b1bdedfb082de3
Chih-Hui Wang <chihhui.x@gmail.com>
2023-05-11 11:43:16 +0800

    [style] prettier

--- server/route/ff/file_restore_route.js
+++ server/route/ff/file_restore_route.js
@@ -8,26 +8,11 @@ import {
 	restoreDeleteValid,
 	ValidCB,
 } from "../../middleware/input_validator.js";
-import {
-	restoreHistory,
-	restoreDeleted,
-} from "../../controller/ff/file_restore_controller.js";
+import { restoreHistory, restoreDeleted } from "../../controller/ff/file_restore_controller.js";
 // ===============================================================================
 
-router.post(
-	"/restore-history",
-	authentication,
-	restoreHistoryValid,
-	ValidCB,
-	restoreHistory
-);
+router.post("/restore-history", authentication, restoreHistoryValid, ValidCB, restoreHistory);
 
-router.post(
-	"/restore-deleted",
-	authentication,
-	restoreDeleteValid,
-	ValidCB,
-	restoreDeleted
-);
+router.post("/restore-deleted", authentication, restoreDeleteValid, ValidCB, restoreDeleted);
 
 export { router as file_restore_route };
