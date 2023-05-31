0176d113822266b97d065e0c38f163fbca58fde4
Chih-Hui Wang <chihhui.x@gmail.com>
2023-05-31 15:33:35 +0800

    [fix] update check usage function in the returnUsage route

--- server/controllers/user/user_usage_controller.js
+++ server/controllers/user/user_usage_controller.js
@@ -1,37 +1,16 @@
 import { CustomError } from "../../utils/custom_error.js";
-// import { generateCurrentTime } from "../../utils/utils.js";
-// import { checkUsedByUser, updateUsedByUser } from "../../models/db_plan.js";
-
+import { getProfile } from "../../models/db_user.js";
 // ===================================================================================
 const returnUsage = async (req, res, next) => {
-	const allocated = req.session.user.allocated;
-	const used = req.session.user.used;
-	if (typeof used === "undefined" || used === null || !allocated) {
-		return next(new CustomError.internalServerError());
+	const profile = await getProfile(req.session.user.id);
+	if (!profile) {
+		return next(CustomError.internalServerError());
 	}
+	const { allocated, used } = profile;
 	return res.json({
 		allocated,
 		used,
 	});
-	// const userId = req.session.user.id;
-	// const allocated = req.session.user.allocated;
-	// const used = req.session.user.used;
-
-	// const currentUsed = await checkUsedByUser(userId);
-	// if (currentUsed === used) {
-	// 	return res.json({
-	// 		allocated,
-	// 		used,
-	// 	});
-	// }
-
-	// const nowTime = generateCurrentTime();
-	// await updateUsedByUser(userId, currentUsed, nowTime);
-	// req.session.user.used = currentUsed;
-	// return res.json({
-	// 	allocated,
-	// 	used: currentUsed,
-	// });
 };
 
 export { returnUsage };
